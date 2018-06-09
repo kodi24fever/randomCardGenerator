fb03449b6a000b5ec6f49e0b20f2b8519aff1fff
Frank Izquierdo <frankdavid93om@yahoo.com>
2018-06-09 01:05:05 +0000

    finished project

--- src/js/index.js
+++ src/js/index.js
@@ -11,4 +11,42 @@ import '../style/index.scss';
 
 window.onload = function(){
     
+    
+    // Creating random number between 1-12 for middle content
+    var addRandomNumber = document.getElementById("number");
+    var randomNumber = Math.floor(Math.random()*12+1);
+    var convertedNumber = randomNumber.toString();
+    addRandomNumber.innerHTML = convertedNumber;
+    
+    //change numbers after 10 to letters
+    if (randomNumber === 11){
+        addRandomNumber.innerHTML = "J";
+    } else if (randomNumber === 12){
+        addRandomNumber.innerHTML = "K";
+    } else if (randomNumber === 1){
+        addRandomNumber.innerHTML = "A";
+    }
+    
+    
+    //Creating top sign of card
+    var randomIcon = Math.floor(Math.random()*4+1);
+    if (randomIcon === 1) {
+        document.getElementById("header1").classList.add("lozenge");
+        document.getElementById("header2").classList.add("lozenge");
+    }else if (randomIcon === 2){
+        document.getElementById("header1").classList.add("redheart");
+        document.getElementById("header2").classList.add("redheart");
+    }else if (randomIcon === 3){
+        document.getElementById("header1").classList.add("blacksuit");
+        document.getElementById("header2").classList.add("blacksuit");
+    } else {
+        document.getElementById("header1").classList.add("spadesuit");
+        document.getElementById("header2").classList.add("spadesuit");
+    }
+    
+  
+    
+    
+    
+
 };
